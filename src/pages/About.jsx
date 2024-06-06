738822ec102d76e434f4dff6c61911f7285a8c17
Julien WH <wh.julien+github.com@gmail.com>
2024-06-06 20:12:26 +0200

    Integration started
    Started mobile first integration of Header and About component.

---
+++ src/pages/About.jsx
@@ -0,0 +1,32 @@
+import './About.scss';
+ 
+import React from 'react';
+
+function About() {
+  return (
+    <section className='aboutSection'>
+      <header><img src="src\assets\images\about-header.jpeg" alt="Une photo d'un paysage avec des montagnes et une forêt de sapins." /></header>
+      <div>
+        <h3>Fiabilité</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id lacinia aliquet, nisl velit tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi. Sed euismod, nisl id lacinia aliquet, nunc nisl tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi.</p>
+      </div>
+
+      <div>
+        <h3>Respect</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id lacinia aliquet, nisl velit tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi. Sed euismod, nisl id lacinia aliquet, nunc nisl tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi.</p>
+      </div>
+
+      <div>
+        <h3>Service</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id lacinia aliquet, nisl velit tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi. Sed euismod, nisl id lacinia aliquet, nunc nisl tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi.</p>
+      </div>
+
+      <div>
+        <h3>Sécurité</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id lacinia aliquet, nisl velit tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi. Sed euismod, nisl id lacinia aliquet, nunc nisl tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi.</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
