3eb47a8cb9d71c39177b944484d767b5b54976b9
Julien WH <wh.julien+github.com@gmail.com>
2024-06-08 09:36:53 +0200

    CSS Modules
    Css modules pour la page "about".

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,41 +1,21 @@
-import './About.scss';
 import Collapsible from '../components/Collapsible/Collapsible';
 import React from 'react';
+import styles from './About.module.scss';
 import data from '../../datas/aboutDatas.json';
 
 function About() {
 
   return (
-    <section className='aboutSection'>
-      <header><img src="src\assets\images\about-header.jpeg" alt="Une photo d'un paysage avec des montagnes et une forêt de sapins." /></header>
+    <section className={styles.section}>
+      <header className={styles.header}><img className={styles.img} src="src\assets\images\about-header.jpeg" alt="Une photo d'un paysage avec des montagnes et une forêt de sapins." /></header>
   
   {/* Ajout des collapsibles avec les données du json */}
   {data.map ((item, index) => (
-    <Collapsible key={index} pageClass="aboutContent" title={item.title}>
+    <Collapsible key={index} pageClass={styles.content} title={item.title}>
       {item.content}
     </Collapsible>
   )
   )}
-
-      {/* <div className='collapsibleDiv'>
-        <h3>Fiabilité</h3>
-        <p></p>
-      </div>
-
-      <div>
-        <h3>Respect</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id lacinia aliquet, nisl velit tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi. Sed euismod, nisl id lacinia aliquet, nunc nisl tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi.</p>
-      </div>
-
-      <div>
-        <h3>Service</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id lacinia aliquet, nisl velit tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi. Sed euismod, nisl id lacinia aliquet, nunc nisl tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi.</p>
-      </div>
-
-      <div>
-        <h3>Sécurité</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id lacinia aliquet, nisl velit tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi. Sed euismod, nisl id lacinia aliquet, nunc nisl tincidunt nunc, vitae tincidunt felis nunc id nunc. Sed vitae nunc sed nunc tincidunt lacinia. Sed id semper nunc. Nulla facilisi.</p>
-      </div> */}
     </section>
   );
 }
