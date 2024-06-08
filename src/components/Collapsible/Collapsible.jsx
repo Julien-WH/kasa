d5bc9279bedcad2146a73aceb5ef042eeb7b43ff
Julien WH <wh.julien+github.com@gmail.com>
2024-06-08 09:51:08 +0200

    CSS Modules
    Utilisation des modules CSS pour le composant Collapsible.

--- src/components/Collapsible/Collapsible.jsx
+++ src/components/Collapsible/Collapsible.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./Collapsible.scss";
+import styles from "./Collapsible.module.scss";
 
 function Collapsible({ title, children, pageClass }) {
   const [isOpen, setIsOpen] = React.useState(false);
   return (
-    <div className={`collapsibleDiv ${pageClass} `}>
-      <button className={isOpen ? "open" : ""} onClick={() => setIsOpen(!isOpen)}>{title}</button>
-      <p className={`content ${isOpen ? "open" : ""}`}>{children}</p>
+    <div className={`${styles.container} ${pageClass} `}>
+      <button className={isOpen ? `${styles.open}` : ""} onClick={() => setIsOpen(!isOpen)}>{title}</button>
+      <p className={`${styles.content} ${isOpen ? `${styles.open}` : ""}`}>{children}</p>
     </div>
   );
 }
