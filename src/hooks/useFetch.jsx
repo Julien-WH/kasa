e1cc18d290ad0b23ed78301cb7d3cb70127686c9
Julien WH <wh.julien+github.com@gmail.com>
2024-06-11 13:00:49 +0200

    code cleaning

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = "/datas/logements.json";
+const API_URL = "../../datas/logements.json";
 
 const useFetch = (id) => {
     const navigate = useNavigate();
