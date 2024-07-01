d9f187dc87d50e7794c3ad90247a3237db7bf9ca
Julien WH <wh.julien+github.com@gmail.com>
2024-07-01 08:33:17 +0200

    Update NotFound404.jsx
    Fix useEffect

--- src/pages/NotFound404/NotFound404.jsx
+++ src/pages/NotFound404/NotFound404.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./NotFound404.module.scss";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 function NotFound404() {
   useEffect(() => {
